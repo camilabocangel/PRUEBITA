dd7381e4c4bfb5930890fc86932ef06630fa7614
cosdealpha <bocangelcamila@gmail.com>
2024-11-28 23:47:28 -0400

    Primer intento login

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.example.pruebitas"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.pruebitas"
         minSdk = 29
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
--- app/src/main/java/com/example/pruebitas/MainActivity.kt
+++ app/src/main/java/com/example/pruebitas/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.pruebitas
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +18,8 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        var usuarioInput : EditText = findViewById(R.id.usuario)
+        var contrasenaInput : EditText = findViewById(R.id.contraseña)
+        var loginBoton : Button = findViewById(R.id.login)
     }
 }
